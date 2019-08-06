03a34d0d64634f34a1f050b3dce3111e6199f75f
Isaac Sears <isaac.j.sears@gmail.com>
2019-08-06 14:00:22 -0400

    Assume connection to a 'refapp' container

--- flask_app.py
+++ flask_app.py
@@ -3,22 +3,29 @@ import requests
 from flask import request
 app = Flask(__name__)
 
-url= 'https://openmrs-staging.callnigeriandoc.com/openmrs/module/idgen/generateIdentifier.form?source=1&username=admin&password=Admin123'
-registerUrl='https://openmrs-staging.callnigeriandoc.com/openmrs/ws/rest/v1/patient'
+openmrs_host = 'http://refapp:8080/'
+idgen_path = 'openmrs/module/idgen/generateIdentifier.form?source=1&username=admin&password=Admin123'
+registeration_path ='openmrs/ws/rest/v1/patient'
+
 username= 'telemedtest3'
 password= 'Asdfg123'
 
 
 @app.route('/fetch')
 def hello_world():
-   response = requests.get(url)
+   response = requests.get(openmrs_host + idgen_path)
    return (response.text)
 
 @app.route('/register',methods=['POST'])
 def register_patient():
-    print(request.get_json(force=True))
-    response = requests.post(registerUrl,auth=(username,password), json=request.get_json(force=True))
-    return response.text
+   print(request.get_json(force=True))
+   response = requests.post(
+      openmrs_host + registeration_path,
+      auth=(username,password), 
+      json=request.get_json(force=True)
+   )
+   
+   return response.text
 
 
 if __name__ == '__main__':
